b700b6797c6af1ac7f165a07b26da593dd55a315
Bennett Dams <dams.bennett@gmail.com>
2021-04-05 19:48:23 +0200

    Integrate bot at server

--- bot.ts
+++
@@ -1,5 +0,0 @@
-import { everyMinute } from 'https://deno.land/x/deno_cron/cron.ts';
-
-everyMinute(() => {
-  console.log("Test log")
-});
--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
 import { serve } from "https://deno.land/std@0.57.0/http/server.ts";
 import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { everyMinute } from 'https://deno.land/x/deno_cron/cron.ts';
+
+function initBot() {
+  everyMinute(() => {
+    console.log(`Test log ${new Date().toISOString()}`)
+  });
+}
 
 const { args } = Deno;
 const DEFAULT_PORT = 8000;
 const argPort = parse(args).port;
 
 const s = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+
+initBot()
 
 for await (const req of s) {
   req.respond({ body: `Hello World\n ${new Date().toISOString()}` });
