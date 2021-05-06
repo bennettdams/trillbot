797f0b2e9a8e0d346d763ffb15063565b6d01cbe
Bennett Dams <dams.bennett@gmail.com>
2021-05-06 22:39:28 +0200

    Initial Puppeteer setup

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { serve } from 'https://deno.land/std/http/server.ts'
+export { parse } from 'https://deno.land/std/flags/mod.ts';
+export { everyMinute, cron } from 'https://deno.land/x/deno_cron/cron.ts';
--- server.ts
+++ server.ts
@@ -1,25 +1,30 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
-import { parse } from 'https://deno.land/std/flags/mod.ts';
-import { everyMinute } from 'https://deno.land/x/deno_cron/cron.ts';
+import { cron, everyMinute, parse, serve } from "./deps.ts";
+import puppeteer from "https://deno.land/x/puppeteer@5.5.1/mod.ts";
 
-function initBot() {
-  everyMinute(() => {
-    console.log(`Test log ${new Date().toISOString()}`)
-  });
+async function initBot() {
+  // everyMinute(() => {
+  //   console.log(`Test log ${new Date().toISOString()}`)
+  // });
+  console.log("Init");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
 }
-
 
 const { args } = Deno;
 const DEFAULT_PORT = 8000;
-const argPort = parse(args).port;
-const serverPort = argPort || DEFAULT_PORT
+const argPort: string = parse(args).port;
+const serverPort = Number(argPort) || DEFAULT_PORT;
 
 const s = serve({ port: serverPort });
 
-initBot()
+initBot();
 
-console.log(`🦕 Deno server running at ${argPort} 🦕`)
+console.log(`🦕 Deno server running at ${serverPort} 🦕`);
 
 for await (const req of s) {
-    req.respond({ body: `Hello World\n ${new Date().toISOString()}` });
+  req.respond({ body: `Hello World\n ${new Date().toISOString()}` });
 }
